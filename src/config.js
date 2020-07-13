8f7a23bee791743a2d7c14c90daa74bc10bcce3d
Taeil Kwak <taeil2@gmail.com>
2020-07-13 02:21:21 -0500

    set api to production

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  // API_ENDPOINT: 'https://ancient-reef-60096.herokuapp.com/api',
+  // API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT: 'https://ancient-reef-60096.herokuapp.com/api',
   TOKEN_KEY: '893hjhks-s87s9fh-8sdfhb-iy8778bhk',
 }
